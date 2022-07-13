ffb1f35bf0db9218954f161f8787575e4bfd89d6
kozmarti <kozmarti@gmail.com>
2022-07-13 18:04:32 +0200

    stable without animated loading

---
+++ app/components/ActivityIndicator.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import LottieView from 'lottie-react-native';
+
+function ActivityIndicator({visible=false}) {
+	if (!visible) return null;
+	return <LottieView loop={true} source={require("../assets/animation/loader.json")}/>
+};
+
+export default ActivityIndicator;
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -7,21 +7,26 @@ import routes from '../navigation/routes';
 import listingsApi from '../api/listings';
 import AppText from '../components/AppText';
 import AppButton from '../components/AppButton';
-
+import ActivityIndicator from '../components/ActivityIndicator';
 
 
 function ListingsScreen({navigation}) {
 	const [listings, setListings] = useState([]);
 	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
 	useEffect(() => {
 		loadListings();
 	}, []);
 	const loadListings = async ()  => {
+		setLoading(true);
 		const response = await listingsApi.getListings();
+		setLoading(false);
+
 		if(!response.ok) return setError(true);
+
 		setError(false);		
 		setListings(response.data);
-	}
+	};
 	
 	return (
 <Screen style={styles.screen}>
@@ -31,17 +36,8 @@ function ListingsScreen({navigation}) {
 		<AppButton title="Retry" onPress={loadListings} color={"secondary"}/>
 		</View>
 		}
-	<FlatList
-	data={listings}
-	keyExtractor={listing => listing.id.toString()}
-	renderItem={({ item }) => 
-	<Card 
-	title={item.title}
-	subTitle={"$" +item.price}
-	imageUrl={item.images[0].url}
-	onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-	/>
-	} />
+		<ActivityIndicator visible={true}/>
+	
 </Screen>
 	);
 }
